f3ced46406303f9edb85b2c4dd8637eaa042dc01
Rodrigo <rodrigoalmeidagit@gmail.com>
2021-06-22 20:43:39 -0300

    add newroom page

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,39 @@
+// Import Assets
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+import illustrationImg from '../assets/images/illustration.svg';
+
+// Import Button Component
+import { Button } from '../components/Button';
+
+// Import CSS Style
+import '../styles/auth.scss';
+
+export function NewRoom() {
+  return (
+    <div className="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Toda pergunta tem <br />uma resposta.</strong>
+        <p>Aprenda e compartilhe conhecimento <br />com outras pessoas</p>
+      </aside>
+
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Logo Letmeask" />
+          <h2>Crie uma nova sala</h2>
+          <form>
+            <input 
+              type="text"
+              placeholder="Nome da sala"
+            />
+            <Button type="submit">
+              Criar sala
+            </Button>
+          </form>
+          <p>Quer entrar em uma sala existente? <a href="#">Clique aqui</a></p>
+        </div>
+      </main>
+    </div>
+  )
+}
