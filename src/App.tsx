93652e50b26aafbd12db4aebe52f94181f1af3e7
Rodrigo <rodrigoalmeidagit@gmail.com>
2021-06-23 11:56:01 -0300

    include switch state

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,21 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // Import Pages
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
+import { Room } from './pages/Room';
 
 // Import Context
 import { AuthContextProvider } from './contexts/AuthContext';
+
 
 function App() {  
   return (
     <BrowserRouter> 
       <AuthContextProvider>
-        <Route path="/" exact component={ Home } />
-        <Route path="/rooms/new" component={ NewRoom } />
+        <Switch>
+          <Route path="/" exact component={ Home } />
+          <Route path="/rooms/new" component={ NewRoom } />
+          <Route path="/rooms/:id" component={ Room } />
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
