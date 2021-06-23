544db544f85341cc11e9f132ac9bdc72c173af8c
Rodrigo <rodrigoalmeidagit@gmail.com>
2021-06-22 22:47:50 -0300

    change export data, firebase and auth path

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -14,5 +14,7 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-export const auth = firebase.auth();
-export const data = firebase.database();
+const auth = firebase.auth();
+const dataBase = firebase.database();
+
+export { firebase, auth, dataBase }
