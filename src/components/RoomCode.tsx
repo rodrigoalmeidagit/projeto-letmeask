34b08b3acadcccfd021cac326ce3a77f64afea93
Rodrigo <rodrigoalmeidagit@gmail.com>
2021-06-24 12:06:34 -0300

    add clipboard function and imports

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -1,14 +1,25 @@
+// Import Img
 import copyImg from '../assets/images/copy.svg';
 
+// Import CSS Style
 import '../styles/room-code.scss';
 
-export function RoomCode() {
+// TypeScript
+type RoomCodeProps = {
+  code: string;
+}
+
+export function RoomCode( props: RoomCodeProps ) {
+  function copyRoomCodeToClipboard() {
+    navigator.clipboard.writeText( props.code )
+  }
+
   return (
-    <button className="room-code">
+    <button className="room-code" onClick={ copyRoomCodeToClipboard }>
       <div>
         <img src={ copyImg } alt="Copy room code" />
       </div>
-      <span>Sala -MctGmVF7KWYAz_6cjT7</span>
+      <span>Sala #{ props.code }</span>
     </button> 
   );
 }
