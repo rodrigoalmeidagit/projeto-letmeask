77604fe87b8cc0f93468c49d7725655993947ccf
Rodrigo <rodrigoalmeidagit@gmail.com>
2021-06-25 10:56:56 -0300

    add adminroom routes and import

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
 
 // Import Context
 import { AuthContextProvider } from './contexts/AuthContext';
-
 
 function App() {  
   return (
@@ -18,6 +18,8 @@ function App() {
           <Route path="/" exact component={ Home } />
           <Route path="/rooms/new" component={ NewRoom } />
           <Route path="/rooms/:id" component={ Room } />
+          <Route path="/admin/rooms/:id" component={ AdminRoom } />
+          
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
